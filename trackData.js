a07ec0ae15099d745e3f83c7774bf3441eb8b934
ThePython10110 <87204246+ThePython10110@users.noreply.github.com>
2026-05-24 22:44:13 -0700

    Add RoR2

--- trackData.js
+++ trackData.js
@@ -1,92 +1,183 @@
 const trackData = [
     {
-        "title": "Risk of Rain (RoRR)",
+        "title": "[RoRR] Risk of Rain",
         "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-engineer-edition-returns-2015/krehkiwzlb/01.%20Risk%20of%20Rain%20Drums%20120.mp3",
         "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-2023/wlqwnwtd/01.%20Risk%20of%20Rain%20%5B2023%20Remaster%5D.mp3"
     },
     {
-        "title": "Cyclogenesis (RoRR)",
+        "title": "[RoRR] Cyclogenesis",
         "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-engineer-edition-returns-2015/wpjipudnsq/05.%20Cyclogenesis%20Drums%20120.mp3",
         "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-2023/jmjinrtv/05.%20Cyclogenesis%20%5B2023%20Remaster%5D.mp3"
     },
     {
-        "title": "Dew Point (RoRR)",
+        "title": "[RoRR] Dew Point",
         "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-engineer-edition-returns-2015/bmxwoxktzv/10.%20Dew%20Point%20Drums%20120.mp3",
         "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-2023/ncxgnxyb/02.%20Dew%20Point%20%5B2023%20Remaster%5D.mp3"
     },
     {
-        "title": "Monsoon (RoRR)",
+        "title": "[RoRR] Monsoon",
         "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-engineer-edition-returns-2015/hjsmxyxmcw/15.%20Monsoon%20Drums%20120.mp3",
         "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-2023/zjuzlihl/04.%20Monsoon%20%5B2023%20Remaster%5D.mp3"
     },
     {
-        "title": "Surface Tension (RoRR)",
+        "title": "[RoRR] Surface Tension",
         "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-engineer-edition-returns-2015/tvdybeamiu/21.%20Surface%20Tension%20Drums%20150.mp3",
         "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-2023/huccfvko/12.%20Surface%20Tension%20%5B2023%20Remaster%5D.mp3"
     },
     {
-        "title": "Moisture Deficit (RoRR)",
+        "title": "[RoRR] Moisture Deficit",
         "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-engineer-edition-returns-2015/tqjwtgpohf/26.%20Moisture%20Deficit%20Drums%20120.mp3",
         "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-2023/mgueluol/08.%20Moisture%20Deficit%20%5B2023%20Remaster%5D.mp3"
     },
     {
-        "title": "Aurora Borealis (RoRR)",
+        "title": "[RoRR] Aurora Borealis",
         "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-engineer-edition-returns-2015/frfqhcxkoo/32.%20Aurora%20Borealis%20Drums%20156.mp3",
         "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-2023/yfhvfqmo/11.%20Aurora%20Borealis%20%5B2023%20Remaster%5D.mp3"
     },
     {
-        "title": "Tropic of Capricorn (RoRR)",
+        "title": "[RoRR] Tropic of Capricorn",
         "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-engineer-edition-returns-2015/nvfiwhdtwy/36.%20Tropic%20of%20Capricorn%20Drums%20160.mp3",
         "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-2023/jskwcwcw/03.%20Tropic%20of%20Capricorn%20%5B2023%20Remaster%5D.mp3"
     },
     {
-        "title": "Arctic Oscillation (RoRR)",
+        "title": "[RoRR] Arctic Oscillation",
         "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-engineer-edition-returns-2015/vylsuxyzuy/41.%20Arctic%20Oscillation%20Drums%20135.mp3",
         "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-2023/nbpxqntd/13.%20Arctic%20Oscillation%20%5B2023%20Remaster%5D.mp3"
     },
     {
-        "title": "Coalescence (RoRR)",
+        "title": "[RoRR] Coalescence",
         "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-engineer-edition-returns-2015/zvgcnvybpz/46.%20Coalescence%20Drums%2090.mp3",
         "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-2023/xjckrdth/16.%20Coalescence%20%5B2023%20Remaster%5D.mp3"
     },
     {
-        "title": "Hailstorm (RoRR)",
+        "title": "[RoRR] Hailstorm",
         "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-engineer-edition-returns-2015/mtizsrhsyh/52.%20Hailstorm%20Drums%20154.675.mp3",
         "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-2023/rqdunhwk/07.%20Hailstorm%20%5B2023%20Remaster%5D.mp3"
     },
     {
-        "title": "Chanson d'Automne (RoRR)",
+        "title": "[RoRR] Chanson d'Automne",
         "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-engineer-edition-returns-2015/jrjmbsvvvw/58.%20Chanson%20d%27Automne%20Drums%2060.mp3",
         "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-2023/fflpqjzu/17.%20Chanson%20d%27Automne..%20%5B2023%20Remaster%5D.mp3"
     },
     {
-        "title": "Tropic of Cancer (RoRR)",
+        "title": "[RoRR] Tropic of Cancer",
         "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-engineer-edition-returns-2015/ntqdlbrvnv/64.%20Tropic%20of%20Cancer%20Drums%20110.mp3",
         "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-2023/hexzeanm/10.%20Tropic%20of%20Cancer%20%5B2023%20Remaster%5D.mp3"
     },
     {
-        "title": "25.3°N 91.7°E (RoRR)",
+        "title": "[RoRR] 25.3°N 91.7°E",
         "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-engineer-edition-returns-2015/yrminnajoz/68.%20Cherrapunji%20Drums%2090.mp3",
         "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-2023/tilfrspx/06.%2025.3%C2%B0N%2091.7%C2%B0E%20%5B2023%20Remaster%5D.mp3"
     },
     {
-        "title": "Precipitation (RoRR)",
+        "title": "[RoRR] Precipitation",
         "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-engineer-edition-returns-2015/uyxhbduzlv/74.%20Precipation%20Drums%20148.mp3",
         "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-2023/luppdzns/14.%20Precipitation%20%5B2023%20Remaster%5D.mp3"
     },
     {
-        "title": "Double ******* Rainbow (RoRR)",
+        "title": "[RoRR] Double ******* Rainbow",
         "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-engineer-edition-returns-2015/jggyeznefu/80.%20Double%20Fucking%20Rainbow%20Drums%2080.mp3",
         "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-2023/xjfwqsos/15.%20Double%20Fucking%20Rainbow%20%5B2023%20Remaster%5D.mp3"
     },
     {
-        "title": "Risk of Rain Returns (RoRR)",
+        "title": "[RoRR] Risk of Rain Returns",
         "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-engineer-edition-returns-2015/hekklwngoc/91.%20R.02%20-%20BUS%201%20DRUMS.mp3",
         "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-2023/yjzxnvpn/19.%20Risk%20of%20Rain%20Returns.mp3"
     },
     {
-        "title": "Double ******* Rainbow Returns (RoRR)",
+        "title": "[RoRR] Double ******* Rainbow Returns",
         "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-engineer-edition-returns-2015/hekklwngoc/91.%20R.02%20-%20BUS%201%20DRUMS.mp3",
         "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-returns-2023/mcavbuwx/21.%20Double%20Fucking%20Rainbow%20Returns.mp3"
+    },
+
+    {
+        "title": "[RoR2] The Dehydration of Risk of Rain 2",
+        "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-2-engineer-edition-2/uokilhaf/001.%20ROR2.01%20%5BLOG%5D%20-%20%CE%A301%20Drums%20I.mp3",
+        "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-2-soundtrack/ohoifvfgrf/11.%20The%20Dehydration%20of%20Risk%20of%20Rain%202.mp3"
+    },
+    { // I *could* have done the other percussion track with the acoustic drums, but I chose not to.
+        "title": "[RoR2] Risk of Rain 2",
+        "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-2-engineer-edition-2/phpbqwny/014.%20ROR2.01%20%5BMENU%5D%20-%20%CE%A302%20Drums%20II%20%26%20Perc.mp3",
+        "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-2-soundtrack/gqtzbnrsix/02.%20Risk%20of%20Rain%202.mp3"
+    },
+    {
+        "title": "[RoR2] Into the Doldrums",
+        "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-2-engineer-edition-2/vthgljpu/026.%20ROR2.02%20-%20%CE%A301%20Drums%20I.mp3",
+        "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-2-soundtrack/famegtzlbq/08.%20Into%20the%20Doldrums.mp3"
+    },
+    {
+        "title": "[RoR2] Parjanya",
+        "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-2-engineer-edition-2/nsawidoj/039.%20ROR2.04%20-%20%CE%A301%20Drums%20I.mp3",
+        "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-2-soundtrack/ehmjyltkwr/12.%20Parjanya.mp3",
+    },
+    {
+        "title": "[RoR2] Thermodynamic Equilibrium",
+        "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-2-engineer-edition-2/mtikzrnm/046.%20ROR2.05%20-%20%CE%A302%20Drums%20II%20%26%20Perc.mp3",
+        "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-2-soundtrack/btkmtvnqlp/04.%20Thermodynamic%20Equilibrium.mp3"
+    },
+    {
+        "title": "[RoR2] Nocturnal Emission",
+        "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-2-engineer-edition-2/fwohucaw/056.%20ROR2.06%20-%20%CE%A301%20Drums%20I.mp3",
+        "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-2-soundtrack/bkqtcjxtsr/10.%20Nocturnal%20Emission.mp3"
+    },
+    {
+        "title": "[RoR2] Evapotranspiration",
+        "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-2-engineer-edition-2/pahliirh/069.%20ROR2.07%20-%20%CE%A301%20Drums%20I.mp3",
+        "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-2-soundtrack/uiyoopiite/03.%20Evapotranspiration.mp3"
+    },
+    {
+        "title": "[RoR2] A Glacier Eventually Farts (And Don't You Listen to the Song of Life)",
+        "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-2-engineer-edition-2/ewtdluol/082.%20ROR2.08%20-%20%CE%A302%20Drums%20II%20%26%20Perc.mp3",
+        "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-2-soundtrack/wygtbxusay/09.%20A%20Glacier%20Eventually%20Farts%20%28And%20Don%27t%20You%20Listen%20to%20the%20Song%20of%20Life%29.mp3"
+    },
+    {
+        "title": "[RoR2] The Raindrop that Fell to the Sky",
+        "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-2-engineer-edition-2/odatopgl/091.%20ROR2.13%20-%20%CE%A301%20Drums%20I.mp3",
+        "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-2-soundtrack/pprmltiqtl/16.%20The%20Raindrop%20that%20Fell%20to%20the%20Sky.mp3"
+    },
+    {
+        "title": "[RoR2] The Rain Formerly Known as Purple",
+        "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-2-engineer-edition-2/ruarkxhv/103.%20ROR2.14%20-%20%CE%A301%20Drums%20I.mp3",
+        "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-2-soundtrack/tmfxzevwpv/15.%20The%20Rain%20Formerly%20Known%20as%20Purple.mp3"
+    },
+    {
+        "title": "[RoR2] Hydrophobia",
+        "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-2-engineer-edition-2/qbjqduly/113.%20ROR2.16%20-%20%CE%A301%20Drums%20I.mp3",
+        "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-2-soundtrack/svfrkrdqhh/13.%20Hydrophobia.mp3"
+    },
+    {
+        "title": "[RoR2] Disdrometer",
+        "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-2-engineer-edition-2/jhhqispu/122.%20ROR2.18%20-%20%CE%A301%20Drums%20I.mp3",
+        "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-2-soundtrack/mjftlzeddd/07.%20Disdrometer.mp3"
+    },
+    {
+        "title": "[RoR2] Terra Pluviam",
+        "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-2-engineer-edition-2/qebsinqq/131.%20ROR2.19%20-%20%CE%A301%20Drums%20I.mp3",
+        "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-2-soundtrack/tonzregkxd/05.%20Terra%20Pluviam.mp3"
+    },
+    {
+        "title": "[RoR2] Köppen As ****",
+        "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-2-engineer-edition-2/ahjztxht/144.%20ROR2.22%20-%20%CE%A301%20Drums%20I.mp3",
+        "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-2-soundtrack/dmxskxofut/06.%20K%C3%B6ppen%20As%20F---.mp3"
+    },
+    {
+        "title": "[RoR2] Antarctic Oscillation",
+        "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-2-engineer-edition-2/orqdwxya/157.%20ROR2.23%20-%20%CE%A301%20Drums%20I.mp3",
+        "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-2-soundtrack/ujqdssyywx/14.%20Antarctic%20Oscillation.mp3"
+    },
+    {
+        "title": "[RoR2] ...con lentitud poderosa",
+        "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-2-engineer-edition-2/luqvhxrr/170.%20ROR2.24%20-%20%CE%A301%20Drums%20I.mp3",
+        "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-2-soundtrack/zxnyucvfaf/18.%20...con%20lentitud%20poderosa.mp3"
+    },
+    {
+        "title": "[RoR2] You're Gonna Need a Bigger Ukulele",
+        "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-2-engineer-edition-2/rbgdyhnw/181.%20ROR2.25%20-%20%CE%A301%20Drums%20I.mp3",
+        "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-2-soundtrack/wokempvkws/17.%20You%27re%20Gonna%20Need%20a%20Bigger%20Ukulele.mp3"
+    },
+    {
+        "title": "[RoR2] Through a Cloud, Darkly",
+        "drums": "https://nu.vgmtreasurechest.com/soundtracks/risk-of-rain-2-engineer-edition-2/xdyjzstn/192.%20ROR2.26%20-%20%CE%A302%20Drums%20II%20%26%20Perc.mp3",
+        "full": "https://jetta.vgmtreasurechest.com/soundtracks/risk-of-rain-2-soundtrack/irmyonewji/01.%20Through%20a%20Cloud%2C%20Darkly.mp3"
     }
 ];
